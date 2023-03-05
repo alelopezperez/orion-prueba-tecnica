7efcc10836a49c1809ce7cffb20522c4a7002010
ale <malejlopez@hotmail.com>
2023-03-05 13:49:09 -0400

    Started Project

---
+++ orion-project-challenge/src/test/java/com/oriontekchallenge/orionprojectchallenge/OrionProjectChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oriontekchallenge.orionprojectchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrionProjectChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
